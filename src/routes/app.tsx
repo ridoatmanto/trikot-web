f032730989576edc6cd41c8907c6c7fc8f3231d4
Rido Atmanto <ridoatmanto@gmail.com>
2024-07-10 22:23:36 +0700

    Update app.tsx

--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -8,30 +8,30 @@ export default function App() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 lg:mx-0 min-h-72">
       <div className="mr-0 lg:mr-4 mb-4">
-        <Card
+{/*         <Card
           title="Todo List"
           tasks={task}
           status="todo"
           stateCallback={setTasks}
-        />
+        /> */}
       </div>
 
       <div className="mr-0 lg:mr-4 mb-4">
-        <Card
+{/*         <Card
           title="In Progress"
           tasks={task}
           status="progress"
           stateCallback={setTasks}
-        />
+        /> */}
       </div>
 
       <div className="mr-0 mb-4">
-        <Card
+{/*         <Card
           title="Done"
           tasks={task}
           status="done"
           stateCallback={setTasks}
-        />
+        /> */}
       </div>
     </div>
   );
