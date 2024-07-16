35057fc94c4b7542fa1476e9084183e256203595
Rido Atmanto <ridoatmanto@gmail.com>
2024-07-16 22:44:44 +0700

    feat: remove unused code

--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -1,61 +1,25 @@
-import { getTasks } from "../storage/tasks.tsx";
 import { ProductCard } from "../components/product-card.tsx";
-import { useState } from "react";
 
 export default function App() {
-  const [task, setTasks] = useState(getTasks());
-
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 lg:mx-0 min-h-72">
       <div className="mr-0 lg:mr-4 mb-4">
-        <ProductCard
-          title="Dortmund Jersey"
-          tasks={task}
-          status="todo"
-          stateCallback={setTasks}
-        />
-      </div>
-
-      <div className="mr-0 lg:mr-4 mb-4">
-        <ProductCard
-          title="Dortmund Jersey"
-          tasks={task}
-          status="todo"
-          stateCallback={setTasks}
-        />
-      </div>
-
-      <div className="mr-0 lg:mr-4 mb-4">
-        <ProductCard
-          title="Dortmund Jersey"
-          tasks={task}
-          status="todo"
-          stateCallback={setTasks}
-        />
+        <ProductCard />
       </div>
       <div className="mr-0 lg:mr-4 mb-4">
-        <ProductCard
-          title="Dortmund Jersey"
-          tasks={task}
-          status="todo"
-          stateCallback={setTasks}
-        />
+        <ProductCard />
       </div>
       <div className="mr-0 lg:mr-4 mb-4">
-        <ProductCard
-          title="Dortmund Jersey"
-          tasks={task}
-          status="todo"
-          stateCallback={setTasks}
-        />
+        <ProductCard />
       </div>
       <div className="mr-0 lg:mr-4 mb-4">
-        <ProductCard
-          title="Dortmund Jersey"
-          tasks={task}
-          status="todo"
-          stateCallback={setTasks}
-        />
+        <ProductCard />
+      </div>
+      <div className="mr-0 lg:mr-4 mb-4">
+        <ProductCard />
+      </div>
+      <div className="mr-0 lg:mr-4 mb-4">
+        <ProductCard />
       </div>
     </div>
   );
--- src/routes/detail.tsx
+++ src/routes/detail.tsx
@@ -1,18 +1,9 @@
-import { getTasks } from "../storage/tasks.tsx";
 import { ProductDetail } from "../components/product-detail.tsx";
-import { useState } from "react";
 
 export function Detail() {
-  const [task, setTasks] = useState(getTasks());
-
   return (
     <div className="flex flex-row justify-center items-center">
-      <ProductDetail
-        title="Dortmund Jersey"
-        tasks={task}
-        status="todo"
-        stateCallback={setTasks}
-      />
+      <ProductDetail />
     </div>
   );
 }
