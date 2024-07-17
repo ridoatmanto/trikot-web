6681c987919cc911ab5e361d270dc98dc7b2aa46
Rido Atmanto <ridoatmanto@gmail.com>
2024-07-17 22:00:22 +0700

    feat: remove link button

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -4,38 +4,18 @@ import { Product } from "../types";
 export function ProductCard({ product }: { product: Product }) {
   return (
     <div className="w-full rounded-lg lg:mb-0 bg-slate-50 dark:bg-slate-700">
-      <div className="text-center text-slate-500 px-2 py-2 dark:text-slate-400 border-b-1 border-dashed border-slate-100 dark:border-slate-50">
-        <img
-          src={`./product-images/${product.image}`}
-          alt={product.name}
-          className="w-42"
-        />
-        <p className="text-md uppercase">{product.name}</p>
-        <h2 className="font-bold text-xl">Rp {product.price}</h2>
-        <Link to={`/detail/${product.id}`}>
-          <button
-            type="submit"
-            className="mt-2 bg-[#AAAAAA] hover:bg-[#B6B6B6] rounded-lg px-1.5 py-1 text-white text-md"
-          >
-            <svg
-              className="inline w-6 h-6 text-white"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7h-1M8 7h-.688M13 5v4m-2-2h4"
-              />
-            </svg>
-            Add to cart
-          </button>
+      <div className="text-center px-2 py-2 dark:text-slate-400 border-b-1 border-dashed border-slate-100 dark:border-slate-50">
+        <Link
+          to={`/detail/${product.id}`}
+          className="text-slate-500 dark:text-slate-400"
+        >
+          <img
+            src={`./product-images/${product.image}`}
+            alt={product.name}
+            className="w-42"
+          />
+          <p className="text-md uppercase">{product.name}</p>
+          <h2 className="font-bold text-xl">Rp {product.price}</h2>
         </Link>
       </div>
     </div>
