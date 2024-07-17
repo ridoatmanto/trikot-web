3c4f713d0a9bd52b92a9b90643639e49f6a3269a
Rido Atmanto <ridoatmanto@gmail.com>
2024-07-17 21:26:55 +0700

    feat: remove static image

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -6,7 +6,7 @@ export function ProductCard({ product }: { product: Product }) {
     <div className="w-full rounded-lg lg:mb-0 bg-slate-50 dark:bg-slate-700">
       <div className="text-center text-slate-500 px-2 py-2 dark:text-slate-400 border-b-1 border-dashed border-slate-100 dark:border-slate-50">
         <img
-          src="./product-images/dortmund-jersey-home-2023-2024.png"
+          src={`./product-images/${product.image}`}
           alt={product.name}
           className="w-42"
         />
