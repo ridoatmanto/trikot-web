0b12a1ee42123471d9e66b956267f5e4feaf1ae0
Rido Atmanto <ridoatmanto@gmail.com>
2024-07-17 21:22:22 +0700

    Delete components directory

--- components/ui/footer.tsx
+++
@@ -1,7 +0,0 @@
-export function Footer() {
-  return (
-    <footer className="text-slate-500 text-xs text-center pb-4 lg:mt-6">
-      Copyright &copy; 2024 <a href="https://ridoatmanto.com">Rido Atmanto</a>
-    </footer>
-  );
-}
