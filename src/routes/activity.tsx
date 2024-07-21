c8b81455457ffde3eb9622858109e9c1fbb26cb4
Rido Atmanto <ridoatmanto@gmail.com>
2024-07-21 11:22:19 +0700

    Delete src/routes/activity.tsx

--- src/routes/activity.tsx
+++
@@ -1,8 +0,0 @@
-export function Activity() {
-  return (
-    <div className="text-slate-500 dark:text-slate-400">
-      <h2 className="text-4xl font-semibold">Your Activity</h2>
-      <p>Here's your activity list</p>
-    </div>
-  );
-}
