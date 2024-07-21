30af046dee702126f76cb405d8ce706cc1d9e107
Rido Atmanto <ridoatmanto@gmail.com>
2024-07-21 12:15:10 +0700

    feat: update attribute

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,10 @@
 export type Product = {
   id: string;
-
   name: string;
   imageURL: string;
   slug: string;
-  image: string;
   price: number;
   description: string;
-
   createdAt: Date;
   updatedAt: Date;
 };
