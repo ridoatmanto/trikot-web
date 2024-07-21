7670d5bcc047ee722816fd8dc9dc4fb037542fd2
Rido Atmanto <ridoatmanto@gmail.com>
2024-07-21 11:24:00 +0700

    Delete src/routes/detail.tsx

--- src/routes/detail.tsx
+++
@@ -1,9 +0,0 @@
-import { ProductDetail } from "../components/product-detail.tsx";
-
-export function Detail() {
-  return (
-    <div className="flex flex-row justify-center items-center">
-      <ProductDetail />
-    </div>
-  );
-}
