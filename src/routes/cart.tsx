5245d2fa693695a5904275d8d0bd36469ed69ff7
Rido Atmanto <ridoatmanto@gmail.com>
2024-08-02 20:47:54 +0700

    fix: cart type error

--- src/routes/cart.tsx
+++ src/routes/cart.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { redirect, useLoaderData } from "react-router-dom";
 import { currencyFormat } from "../libs/currency-format";
 
-import { Cart } from "../types";
+import { Cart, CartItem } from "../types";
 import { BACKEND_API_URL } from "../libs/env";
 import { auth } from "../libs/auth";
 import { Trash2 } from "lucide-react";
@@ -76,7 +76,7 @@ export function CartRoute() {
               </tr>
             </thead>
             <tbody>
-              {data.cart.cart.items.map((item) => (
+              {data.cart.cart.items.map((item: any) => (
                 <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                   <td className="w-4 p-4">
                     <div className="flex items-center">
@@ -124,6 +124,38 @@ export function CartRoute() {
                   </td>
                 </tr>
               ))}
+              {/* <tr className="text-right bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <td className="p-4" colSpan={3}>
+                  <h2 className="text-2xl font-semibold mt-2 mr-4">
+                    Total Payment: Rp. 650.000,-
+                  </h2>
+                  <Link to="/order">
+                    <button
+                      type="submit"
+                      className="w-1/2 mt-2 bg-[#AAAAAA] hover:bg-[#B6B6B6] rounded-lg px-1.5 py-1 text-white text-xl"
+                    >
+                      <svg
+                        className="inline w-6 h-6 text-white -mt-2"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="48"
+                        height="36"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          stroke="currentColor"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M8 17.345a4.76 4.76 0 0 0 2.558 1.618c2.274.589 4.512-.446 4.999-2.31.487-1.866-1.273-3.9-3.546-4.49-2.273-.59-4.034-2.623-3.547-4.488.486-1.865 2.724-2.899 4.998-2.31.982.236 1.87.793 2.538 1.592m-3.879 12.171V21m0-18v2.2"
+                        />
+                      </svg>
+                      Order Now
+                    </button>
+                  </Link>
+                </td>
+              </tr> */}
             </tbody>
           </table>
         </div>
